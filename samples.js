2663c667e9eb454cb298334c34ef3f4df3a5e57b
creampx <mapinxue@qq.com>
2024-03-06 14:19:37 +0800

    main

---
+++ samples.js
@@ -0,0 +1,20 @@
+
+() => {
+    return new Promise((resolve,reject) => {
+        resolve({
+            // token: 'example'
+            uuid: 'ddd',
+            img: 'https....',
+            item_list: [
+                {
+                    item_id: '12333',
+                    item_name: 'name1'
+                },
+                {
+                    item_id: '12333',
+                    item_name: 'name1'
+                }
+            ]
+        })
+    })
+}
